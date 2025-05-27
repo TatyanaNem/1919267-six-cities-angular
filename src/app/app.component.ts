630bdc4c6520050005f02f2ea5915c6296c6b1a3
Tatyana Nemchina <23angorka07@gmail.com>
2025-05-27 14:39:39 +0300

    feat: added pages components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, MainPageComponent],
+  template: ` <app-main-page></app-main-page>`,
 })
-export class AppComponent {
-  title = 'angular-template-project';
-}
+export class AppComponent {}
