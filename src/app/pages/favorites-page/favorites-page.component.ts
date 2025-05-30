79f41f9ab25672650aa5cbe36008bdd5689f1bee
Tatyana Nemchina <23angorka07@gmail.com>
2025-05-30 12:39:43 +0300

    feat: added footer component

--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-favorites-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css',
 })
