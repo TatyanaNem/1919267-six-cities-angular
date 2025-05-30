f7f943ad846d3a71a93f35d6fa5ff4830adb24fd
Tatyana Nemchina <23angorka07@gmail.com>
2025-05-30 10:00:40 +0300

    feat: added header component

--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-favorites-page',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './favorites-page.component.html',
-  styleUrl: './favorites-page.component.css'
+  styleUrl: './favorites-page.component.css',
 })
-export class FavoritesPageComponent {
-
-}
+export class FavoritesPageComponent {}
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { Offer } from '../../../types/offer';
 import { OfferCardComponent } from './components/offer-card/offer-card.component';
 import { offers } from '../../../mocks/offers';
-import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [OfferCardComponent, RouterLink],
+  imports: [OfferCardComponent, HeaderComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
