2973694f20b41ad499e8dc2871eac688a817d298
Tatyana Nemchina <23angorka07@gmail.com>
2025-06-05 19:14:07 +0300

    feat: added grouping offers by location on favorites page

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthService {
-  isAuth = false;
+  isAuth = true;
 
   isAuthenticated() {
     return this.isAuth;
--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { Offer } from '../../shared/types/offer';
+import { offers } from '../../../mocks/offers';
+import { OfferCardComponent } from '../../features/main-block/components/offer-card/offer-card.component';
 
 @Component({
   selector: 'app-favorites-page',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, OfferCardComponent],
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css',
 })
-export class FavoritesPageComponent {}
+export class FavoritesPageComponent implements OnInit {
+  favoriteOffers: [string, Offer[]][] = [];
+  allOffers: Offer[] = offers;
+
+  groupOffersByLocation(items: Offer[]): Record<string, Offer[]> {
+    return items.reduce<Record<string, Offer[]>>((acc, current) => {
+      const location = current.city.name;
+      if (!(location in acc)) {
+        acc[location] = [];
+      }
+      acc[location].push(current);
+
+      return acc;
+    }, {});
+  }
+
+  ngOnInit(): void {
+    this.favoriteOffers = Object.entries(
+      this.groupOffersByLocation(
+        this.allOffers.filter((offer: Offer) => offer.isFavorite)
+      )
+    );
+  }
+}
