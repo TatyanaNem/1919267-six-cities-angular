f426ada81c280ecb33de306d0c1ce3d31e53d1fa
Tatyana Nemchina <23angorka07@gmail.com>
2025-06-10 12:29:34 +0300

    feat: added review form in template-driven mode

--- src/app/features/reviews-block/components/review-form/review-form.component.ts
+++ src/app/features/reviews-block/components/review-form/review-form.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
-import { FormRatingComponent } from '../form-rating/form-rating.component';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-review-form',
-  imports: [FormRatingComponent],
+  imports: [FormsModule],
   templateUrl: './review-form.component.html',
   styleUrl: './review-form.component.css',
 })
 export class ReviewFormComponent {
-  rating = 0;
+  @ViewChild('reviewForm') form: NgForm | undefined;
 
-  onRatingChanged(value: number) {
-    this.rating = value;
+  reviewFormData = {
+    rating: 0,
+    review: '',
+  };
+
+  readonly ratingArray = [
+    { title: 'perfect', value: 5 },
+    { title: 'good', value: 4 },
+    { title: 'not bad', value: 3 },
+    { title: 'badly', value: 2 },
+    { title: 'terribly', value: 1 },
+  ];
+
+  onSubmit() {
+    this.reviewFormData = this.form?.value;
+    if (this.form?.submitted) {
+      this.form.reset();
+    }
   }
 }
